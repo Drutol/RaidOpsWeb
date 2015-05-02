af58ff044152a886bdd5adb59fa5a555c4719bc2
Mordonus <dogier140@poczta.fm>
2015-05-02 19:36:29 +0200

    SMTP key hidden

--- config/environment.rb
+++ config/environment.rb
@@ -9,5 +9,5 @@ ActionMailer::Base.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
   :port      => 587,
   :user_name => 'dogier140@poczta.fm',
-  :password  => 'SgajFCY9dOt6wJcJgL7dKw'
+  :password  => ENV['SMTP_KEY']
 }
