a3d8d2d8f7531a9e122f96f7e6401780d5886a24
Mordonus <dogier140@poczta.fm>
2015-05-25 13:30:05 +0200

    Mailer link addr fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def activation_needed_email(user)
     @user = user
-    @url  = "http://raid-ops-web.herokuapp.com/users/#{user.activation_token}/activate"
+    @url  = "http://148.251.194.214:9292/users/#{user.activation_token}/activate"
     mail(:to => user.email,
          :subject => "Welcome to RaidOpsWeb")
   end
@@ -21,7 +21,7 @@ class UserMailer < ApplicationMailer
 
   def reset_password_email(user)
     @user = User.find user.id
-    @url  = "http://raid-ops-web.herokuapp.com/password_resets/#{@user.reset_password_token}/edit"
+    @url  = "http://148.251.194.214:9292/password_resets/#{@user.reset_password_token}/edit"
     mail(:to => user.email,
          :subject => "Your RaidOpsWeb password has been reset")
   end
