3ed0338e898ec231bd7326ebac243d0b3a9c896c
Mordonus <dogier140@poczta.fm>
2015-06-09 08:17:02 +0200

    Favicon

--- app/controllers/guilds_controller.rb
+++ app/controllers/guilds_controller.rb
@@ -4,7 +4,7 @@ class GuildsController < ApplicationController
 
 	skip_before_filter :require_login, only: [:index, :show,:items_all,:download,:recent_activity,:attendance]
  	before_filter do
-	    if request.host != "raidops.net" && Rails.env.production? then redirect_to "http://raidops.net" end
+	    if request.host != "www.raidops.net" && Rails.env.production? then redirect_to "http://raidops.net" end
             if request.ssl? && Rails.env.production?
 	      redirect_to :protocol => 'http://', :status => :moved_permanently
 	    end
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,7 @@
 class IndexController < ApplicationController
 	skip_before_filter :require_login
 	before_filter do
-	     if request.host != "raidops.net" && Rails.env.production? then redirect_to "http://raidops.net" end
+	     if request.host != "www.raidops.net" && Rails.env.production? then redirect_to "http://raidops.net" end
 	     if request.ssl? && Rails.env.production?
 	      redirect_to :protocol => 'http://', :status => :moved_permanently
 	    end
